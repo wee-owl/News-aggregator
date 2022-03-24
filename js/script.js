6f83d9ef06bb641f98dabfd1d0c5a10b31bf76e6
LenOk <95621680+wee-owl@users.noreply.github.com>
2022-03-24 15:30:42 +0300

    docs: add new file

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const API_KEY = 'b94e0a6c60784e9a9f4bd2410a55d4fe';
+
+const choicesElem = document.querySelector('.js-choice');
+
+const newsList = document.querySelector('.news-list');
+
+const choices = new Choices(choicesElem, {
+    searchEnabled: false,
+    itemSelectText: '',
+});
+
+const getdata = async (url) => {
+    const response = await fetch(url, {
+        headers: {
+            'X-Api-Key': API_KEY,
+        }
+    });
+
+    const data = await response.json();
+
+    return data;
+};
+
+const renderCard = (data) => {
+    newsList.textContent = '';
+    data.forEach(news => {
+        const card = document.createElement('li');
+        card.className = 'news-item';
+
+        card.innerHTML = `
+                        <img src="${news.urlToImage}" alt="${news.title}" class="news-image" width="270" height="200">
+                        <h3 class="news-title">
+                        <a href="${news.url}" class="news-link" target="_blank">${news.title}</a>
+                        </h3>
+                        <p class="news-description">${news.description}</p>
+                        <div class="news-footer">
+                        <time class="news-datetime" datetime="${news.publishedAt}">
+                        <span class="news-date">${news.publishedAt}</span> 11:06
+                        </time>
+                        <div class="news-author">${news.author}</div>
+                        </div>
+        `;
+        
+        newsList.append(card);
+    })
+};
+
+const loadNews = async () => {
+    const data = await getdata('https://newsapi.org/v2/top-headlines?country=ru');
+    renderCard(data.articles);
+};
+
+loadNews();
+
