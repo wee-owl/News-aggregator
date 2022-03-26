b693369f443aaec8ca27b9efcca7d31596d6742b
LenOk <95621680+wee-owl@users.noreply.github.com>
2022-03-26 13:38:22 +0300

    feat: update file

--- js/script.js
+++ js/script.js
@@ -4,20 +4,28 @@ const newsList = document.querySelector('.news-list');
 const formSearch = document.querySelector('.form-search');
 const title = document.querySelector('.title');
 
+const declOfNum = (n, titles) => titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
 const choices = new Choices(choicesElem, {
     searchEnabled: false,
     itemSelectText: '',
 });
 
-const getdata = async (url) => {
-    const response = await fetch(url, {
+const getdata = (error, url) =>
+    fetch(url, {
         headers: {
             'X-Api-Key': API_KEY,
-        }
-    });
-    const data = await response.json();
-    return data;
-};
+        },
+    })
+        .then(response => {
+            console.log('response: ', response);
+            if (!response.ok) {
+                throw error(new Error(response.status))
+            }
+            return response.json();
+        })
+        .catch(error);
 
 const getDateCorrectFormat = isoDate => {
     const date = new Date(isoDate);
@@ -38,13 +46,16 @@ const getDateCorrectFormat = isoDate => {
 const getImage = url => new Promise((resolve) => {
     const image = new Image(270, 200);
 
+
     image.addEventListener('load', () => {
         resolve(image);
     });
+
     image.addEventListener('error', () => {
         image.src = 'image/no-photo.jpg';
         resolve(image);
     });
+
     image.src = url || 'image/no-photo.jpg';
     image.className = 'news-image';
     return image;
@@ -77,19 +88,30 @@ const renderCard = (data) => {
     });
 };
 
+const showError = (err) => {
+    console.warn(err);
+    newsList.textContent = '';
+    title.textContent = `Произошла ошибка, попробуйте позже`;
+    title.classList.remove('hide');
+}
+
 const loadNews = async () => {
     newsList.innerHTML = '<li class="preload"></li>';
     const country = localStorage.getItem('country') || 'ru';
     choices.setChoiceByValue(country);
     title.classList.add('hide');
-    const data = await getdata(`https://newsapi.org/v2/top-headlines?country=${country}&pageSize=100`);
+    const data = await getdata(showError, `https://newsapi.org/v2/top-headlines?country=${country}&pageSize=100`);
     renderCard(data.articles);
 };
 
 const loadSearch = async value => {
-    const data = await getdata(`https://newsapi.org/v2/everything?q=${value}&pageSize=100`);
+    newsList.innerHTML = '<li class="preload"></li>';
+    const data = await getdata(showError, `https://newsapi.org/v2/everything?q=${value}&pageSize=100`);
     title.classList.remove('hide');
-    title.textContent = `По вашему запросу "${value}" найдено ${data.articles.length} результатов`;
+    const arrStr1 = ['найден', 'найдено', 'найдено'];
+    const arrStr2 = ['результат', 'результата', 'результатов'];
+    const count = data.articles.length;
+    title.textContent = `По вашему запросу "${value}" ${declOfNum(count, arrStr1)} ${count} ${declOfNum(count, arrStr2)}`;
     choices.setChoiceByValue('');
     renderCard(data.articles);
 };
@@ -107,5 +129,3 @@ formSearch.addEventListener('submit', event => {
 });
 
 loadNews();
-
-
